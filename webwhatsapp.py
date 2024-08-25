a19d20f0feec27a7e16e6c710853bd0b81c42d55
Keyaan <88016629+Keyaan-07@users.noreply.github.com>
2024-08-25 11:29:16 +0530

    Update webwhatsapp.py

--- webwhatsapp.py
+++ webwhatsapp.py
@@ -8,37 +8,37 @@ from selenium.webdriver.common.keys import Keys
 
 options = Options()
 
-SERVER = 'localhost'
-DATABASE = 'wmessages'
-USERNAME = 'root'
-PASSWORD = ''
+SERVER = 'yooooooooooooo'
+DATABASE = 'wam'
+USERNAME = 'iampowerful'
+PASSWORD = 'supersecretpassword'
 
 
-connectionString = f'DRIVER={{SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
+connectionString = f'DRIVER={"ODBC Driver 17 for SQL Server"};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD};Trusted_Connection=yes;'
+
 
 
 if __name__ == "__main__":
-    print("w")
+    #console.elevate()
     conn = pyodbc.connect(connectionString)
     SQL_QUERY = "SELECT * FROM WhatsAppMessages where sentStatus is null"
     cursor = conn.cursor()
     cursor.execute(SQL_QUERY)
     records = cursor.fetchall()
-    
     for r in records:
         now = datetime.now()
         dttm = now.strftime("%Y%m%d, %H%M%S")
         mssg = r.messagetext + " " + dttm
         phn = "91" + r.receiver
-        print(r.reciever, mssg)
+        print(f"{r.receiver}\t{mssg}")
         
         options.add_argument("-profile")
-        options.add_argument("~/.mozilla/firefox/12g5912d.default-esr")
+        options.add_argument("C:/Users/Irshad Khatri/AppData/Roaming/Mozilla/Firefox/Profiles/rlyugxw5.default-release-1724559328504")
         driver = webdriver.Firefox(options=options)
 
         driver.get("https://web.whatsapp.com/send?phone=" + phn)
 
-        time.sleep(20)
+        time.sleep(30)
         message_box = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[5]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div[2]/div[1]")
 
         wamessage = mssg
@@ -49,4 +49,3 @@ if __name__ == "__main__":
         message_box.send_keys(Keys.RETURN)
         time.sleep(5)
         driver.quit()
-    
